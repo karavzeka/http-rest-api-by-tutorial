7923e222cee77be4a65b0eae483b9534a6395d78
e.karavskii <karavzeka@gmail.com>
2020-06-12 16:55:34 +0300

    chapter 2, database

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/BurntSushi/toml"
+	"log"
+	"flag"
+
+	"github.com/karavzeka/http-rest-api-by-tutorial/internal/app/apiserver"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
+
+func main () {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
